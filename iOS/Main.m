a207f340d9dfea298ef86394221001f7763d1321
here2322 <Adamabdeldayem87@gmail.com>
2026-07-20 17:05:10 -0500

    Change API_URL to point to new server address

--- iOS/Main.m
+++ iOS/Main.m
@@ -3,7 +3,7 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#define API_URL @"https://api.novafn.dev"
+#define API_URL @"http://209.25.140.17:1818"
 #define EPIC_GAMES_URL @"ol.epicgames.com"
 
 @interface CustomURLProtocol : NSURLProtocol
